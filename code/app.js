084245ab3b2658c3de089475f938daecee2400ad
Ceri <ceriwoolway@gmail.com>
2023-04-28 20:46:16 +0100

    Update timer with frames and seconds

--- code/app.js
+++ code/app.js
@@ -1,10 +1,21 @@
 "use strict";
 /* HELLO */
 const timer = document.querySelector('#timer');
+const framesDisplay = document.querySelector('#frames');
+const secondsDisplay = document.querySelector('#seconds');
 let currentFrames = 0;
+let currentSeconds = 0;
 // We are running at 60fps
-const updateTimer = () => {
+const updateFrames = () => {
     currentFrames++;
-    timer.textContent = currentFrames.toString();
+    // timer.textContent = currentFrames.toString();
+    framesDisplay.textContent = `${Math.floor(currentFrames)}`;
 };
-window.setInterval(updateTimer, 1000 / 60);
+const updateTime = () => {
+    currentSeconds++;
+    secondsDisplay.textContent = `
+    ${Math.floor(currentSeconds / 60)} : ${String(currentSeconds % 60).padStart(2, '0')}
+  `;
+};
+window.setInterval(updateFrames, 1000 / 60); // Update the frames every 1/60th of a second
+window.setInterval(updateTime, 1000);
--- code/app.ts
+++ code/app.ts
@@ -1,12 +1,25 @@
 /* HELLO */
 
 const timer: HTMLDivElement = document.querySelector('#timer')!;
+const framesDisplay: HTMLDivElement = document.querySelector('#frames')!;
+const secondsDisplay: HTMLDivElement = document.querySelector('#seconds')!;
+
 let currentFrames: number = 0;
+let currentSeconds: number = 0;
 
 // We are running at 60fps
-const updateTimer = () => {
+const updateFrames = () => {
   currentFrames++;
-  timer.textContent = currentFrames.toString();
+  // timer.textContent = currentFrames.toString();
+  framesDisplay.textContent = `${Math.floor(currentFrames)}`;
 };
 
-window.setInterval(updateTimer, 1000 / 60);
+const updateTime = () => {
+  currentSeconds++;
+  secondsDisplay.textContent = `
+    ${Math.floor(currentSeconds / 60)} : ${String(currentSeconds % 60).padStart(2, '0')}
+  `;
+};
+
+window.setInterval(updateFrames, 1000 / 60); // Update the frames every 1/60th of a second
+window.setInterval(updateTime, 1000);
