fcc0ff10097f5dc0f7160a64cae814b5d1391818
Ceri <ceriwoolway@gmail.com>
2023-04-28 21:02:03 +0100

    Display the in-game hour

--- code/app.js
+++ code/app.js
@@ -1,8 +1,11 @@
 "use strict";
 /* HELLO */
+/* Time related page elements */
 const timer = document.querySelector('#timer');
 const framesDisplay = document.querySelector('#frames');
 const secondsDisplay = document.querySelector('#seconds');
+const inGameHourDisplay = document.querySelector('#in-game-hour');
+/* Time related variables */
 let currentFrames = 0;
 let currentSeconds = 0;
 // We are running at 60fps
@@ -15,10 +18,15 @@ const updateTime = () => {
     secondsDisplay.textContent = `
     ${Math.floor(currentSeconds / 60)} : ${String(currentSeconds % 60).padStart(2, '0')}
   `;
+    let myHour = Math.floor((currentSeconds - 1) / 90);
+    if (!myHour) {
+        myHour = 12;
+    }
+    inGameHourDisplay.textContent = `${myHour.toString()}AM`;
     if (currentSeconds === 855) {
         clearInterval(timeUpdate);
         clearInterval(frameUpdate);
     }
 };
 const frameUpdate = window.setInterval(updateFrames, 1000 / 60); // Update the frames every 1/60th of a second
-const timeUpdate = window.setInterval(updateTime, 1000);
+const timeUpdate = window.setInterval(updateTime, 1000); // Update the seconds every second.
--- code/app.ts
+++ code/app.ts
@@ -1,9 +1,12 @@
 /* HELLO */
 
+/* Time related page elements */
 const timer: HTMLDivElement = document.querySelector('#timer')!;
 const framesDisplay: HTMLDivElement = document.querySelector('#frames')!;
 const secondsDisplay: HTMLDivElement = document.querySelector('#seconds')!;
+const inGameHourDisplay: HTMLDivElement = document.querySelector('#in-game-hour')!;
 
+/* Time related variables */
 let currentFrames: number = 0;
 let currentSeconds: number = 0;
 
@@ -19,6 +22,13 @@ const updateTime = () => {
     ${Math.floor(currentSeconds / 60)} : ${String(currentSeconds % 60).padStart(2, '0')}
   `;
 
+  let myHour = Math.floor((currentSeconds - 1) / 90);
+  if (!myHour) {
+    myHour = 12;
+  }
+
+  inGameHourDisplay.textContent = `${myHour.toString()}AM`;
+
   if (currentSeconds === 855) {
     clearInterval(timeUpdate);
     clearInterval(frameUpdate);
@@ -26,4 +36,4 @@ const updateTime = () => {
 };
 
 const frameUpdate = window.setInterval(updateFrames, 1000 / 60); // Update the frames every 1/60th of a second
-const timeUpdate = window.setInterval(updateTime, 1000);
+const timeUpdate = window.setInterval(updateTime, 1000); // Update the seconds every second.
