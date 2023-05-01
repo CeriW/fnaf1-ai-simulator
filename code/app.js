e466c12d96462a3121c7bce332c9f61d231aa339
Ceri <ceriwoolway@gmail.com>
2023-05-01 14:35:56 +0100

    Make real time and in-game time align

--- code/app.js
+++ code/app.js
@@ -18,8 +18,11 @@ export const animatronics = [
 ];
 // import { Animatronic, animatronics } from './animatronics';
 /* Time related variables */
-let currentFrames = 0;
+let currentFrame = 0;
+let currentSecond = -3; // We start at -1 as 12AM is 89 real seconds long whereas all the others are 90 seconds
+let currentGameHour;
 let framesPerSecond = 60;
+let secondLength = 1; // How long we want a real life 'second' to be in milliseconds. Used to speed up testing.
 /* Time related page elements */
 const framesDisplay = document.querySelector('#frames');
 const secondsDisplay = document.querySelector('#real-time');
@@ -27,37 +30,38 @@ const inGameHourDisplay = document.querySelector('#in-game-time');
 const simulator = document.querySelector('#simulator');
 // ========================================================================== //
 // TIMER BASED FUNCTIONS
+// These are split off separately as they each need to update at
+// different rates.
 // ========================================================================== //
 // We are running at 60fps
 const updateFrames = () => {
-    currentFrames++;
-    framesDisplay.textContent = `${Math.floor(currentFrames)} frames at ${framesPerSecond}fps`;
-    updateRealTime();
-    updateInGameTime();
-    // Make it stop at 6AM
-    if (currentFrames >= 32100) {
-        clearInterval(frameUpdate);
-    }
+    currentFrame++;
+    framesDisplay.textContent = `${currentFrame} frames at ${framesPerSecond}fps`;
 };
-const updateRealTime = () => {
-    let seconds = Math.floor(currentFrames / 60);
-    let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = seconds % 60;
+const updateTime = () => {
+    currentSecond++;
+    // REAL TIME
+    let realMinutes = Math.floor(currentSecond / 60);
+    let realRemainingSeconds = currentSecond % 60;
     secondsDisplay.textContent = `
-    ${minutes} : ${String(remainingSeconds).padStart(2, '0')}
+    ${realMinutes} : ${String(realRemainingSeconds).padStart(2, '0')}
   `;
-};
-// One in game hour is 90 real-life seconds
-const updateInGameTime = () => {
-    let minutes = Math.floor(currentFrames / (60 * 1.5)) > 0 ? Math.floor((currentFrames - 60) / (60 * 1.5)) : 0;
-    let hours = Math.floor(minutes / 60) > 0 ? Math.floor(minutes / 60) : 12;
-    let remainingMinutes = minutes % 60;
+    // IN GAME TIME
+    // One in game hour is 90 real-life seconds (with the exception of 12AM which is 89 seconds)
+    let inGameMinutes = Math.ceil(currentSecond * 0.666666666667) > 0 ? Math.ceil(currentSecond * 0.666666666667) : 0;
+    let inGameHours = Math.floor(inGameMinutes / 60) > 0 ? Math.floor(inGameMinutes / 60) : 12;
+    let inGameRemainingMinutes = inGameMinutes % 60;
     inGameHourDisplay.innerHTML = `
-
-    <span class="in-game-hour">${hours}</span>
-    <span class="in-game-minutes">${String(remainingMinutes).padStart(2, '0')}</span>
+    <span class="in-game-hour">${inGameHours}</span>
+    <span class="in-game-minutes">${String(inGameRemainingMinutes).padStart(2, '0')}</span>
     <span class="am-marker">AM</span>
   `;
+    console.log(`Real time: ${realMinutes}:${realRemainingSeconds} (${currentSecond})     In-game time: ${inGameHours}:${String(inGameRemainingMinutes).padStart(2, '0')}`);
+    updateFrames();
+    if (currentSecond === 539) {
+        clearInterval(timeUpdate);
+        clearInterval(frameUpdate);
+    }
 };
 // ========================================================================== //
 // ANIMATRONIC BASED FUNCTIONS
@@ -75,5 +79,6 @@ const generateAnimatronics = () => {
 // ========================================================================== //
 // INITIALISE THE PAGE
 // ========================================================================== //
-const frameUpdate = window.setInterval(updateFrames, 1000 / framesPerSecond); // Update the frames every 1/60th of a second
+const timeUpdate = window.setInterval(updateTime, secondLength); // Update the frames every 1/60th of a second
+const frameUpdate = window.setInterval(updateFrames, secondLength / framesPerSecond);
 generateAnimatronics();
